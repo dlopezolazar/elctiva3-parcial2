c578629faedd61998f810bfbb923875b52e19dc3
dlopezolazar <dlopez@consultoresinformaticos.com.py>
2018-05-27 11:19:42 -0400

    Ajustes del pom para desplegar proyecto

--- gestion-taller/src/main/java/py/pol/una/electiva3/controller/VehiculoController.java
+++ gestion-taller/src/main/java/py/pol/una/electiva3/controller/VehiculoController.java
@@ -1,7 +1,12 @@
 package py.pol.una.electiva3.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.websocket.server.PathParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,23 +17,62 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import py.pol.una.electiva3.entity.Vehiculo;
+import py.pol.una.electiva3.model.ClienteModel;
+import py.pol.una.electiva3.model.VehiculoModel;
+import py.pol.una.electiva3.repository.VehiculoRepository;
+
 @RestController
 @RequestMapping("/vehiculo")
 public class VehiculoController {
 	
 	private static final String VEHICLE = "{/idVehicle}";
 	
+	@Autowired
+	private VehiculoRepository repository;
+	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> listAllVehicle(){
 		
+		List<VehiculoModel> model = new ArrayList<>();
 		
-		return null;
+		List<Vehiculo> listVehicle = (List<Vehiculo>) repository.findAll();
+		
+		if(listVehicle.isEmpty()){
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		
+		for (Vehiculo vehiculo : listVehicle) {
+			
+			model.add(new VehiculoModel(vehiculo.getVehiculoPK().getMatricula(),
+					vehiculo.getVehiculoPK().getDni(),
+					vehiculo.getModelo(),
+					vehiculo.getColor(),
+					vehiculo.getFechaentrada(),
+					new ClienteModel(vehiculo.getCliente().getDni(),
+							vehiculo.getCliente().getNombre(),
+							vehiculo.getCliente().getApellido(),
+							vehiculo.getCliente().getDireccion(),
+							vehiculo.getCliente().getTelefono())));
+		}
+		
+		return new ResponseEntity<List<VehiculoModel>>(model, HttpStatus.OK);
 	}
 	
 	@GetMapping(value = VEHICLE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> getVehicle(@PathParam("idVehicle") Integer idVehicle){
+	public ResponseEntity<?> getVehicle(@PathParam("idVehicle") String matricula){
 		
-		
+//		Vehiculo vehiculo = repository.findOne(idVehicle);
+//		VehiculoModel model = new VehiculoModel(vehiculo.getVehiculoPK().getMatricula(),
+//				vehiculo.getVehiculoPK().getDni(),
+//				vehiculo.getModelo(),
+//				vehiculo.getColor(),
+//				vehiculo.getFechaentrada(),
+//				new ClienteModel(vehiculo.getCliente().getDni(),
+//						vehiculo.getCliente().getNombre(),
+//						vehiculo.getCliente().getApellido(),
+//						vehiculo.getCliente().getDireccion(),
+//						vehiculo.getCliente().getTelefono()));
 		return null;
 	}
 	
